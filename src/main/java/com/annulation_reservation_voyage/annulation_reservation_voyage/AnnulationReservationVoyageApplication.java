03efa1e2a2831569ad8d0fab35a7d28deb3ced7c
TchassiDaniel <tchassidaniel@gmail.com>
2024-10-28 14:48:07 +0100

    first commit

---
+++ src/main/java/com/annulation_reservation_voyage/annulation_reservation_voyage/AnnulationReservationVoyageApplication.java
@@ -0,0 +1,13 @@
+package com.annulation_reservation_voyage.annulation_reservation_voyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnulationReservationVoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnulationReservationVoyageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/annulation_reservation_voyage/annulation_reservation_voyage/AnnulationReservationVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annulation_reservation_voyage.annulation_reservation_voyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnulationReservationVoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
