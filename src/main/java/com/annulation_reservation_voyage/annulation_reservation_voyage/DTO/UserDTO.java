5f787454ae5da0b70c5f2f80d2f155411012e6d0
Tchassi Daniel <tchassidaniel@gmail.com>
2024-11-22 11:23:48 +0100

    update swagger and add userdto

---
+++ src/main/java/com/annulation_reservation_voyage/annulation_reservation_voyage/DTO/UserDTO.java
@@ -0,0 +1,25 @@
+package com.annulation_reservation_voyage.annulation_reservation_voyage.DTO;
+
+import java.util.UUID;
+
+import com.annulation_reservation_voyage.annulation_reservation_voyage.enums.RoleType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+  private String nom;
+  private String prenom;
+  private String email;
+  private String password;
+  private String telNumber;
+  private RoleType role;
+
+  // Parametre pour un usager
+  private String address;
+
+  // Parametre pour une agence
+  private UUID idcoordonneeGPS;
+}
--- src/main/java/com/annulation_reservation_voyage/annulation_reservation_voyage/controllers/UtilisateurController.java
+++ src/main/java/com/annulation_reservation_voyage/annulation_reservation_voyage/controllers/UtilisateurController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.annulation_reservation_voyage.annulation_reservation_voyage.DTO.AuthentificationDTO;
+import com.annulation_reservation_voyage.annulation_reservation_voyage.DTO.UserDTO;
 import com.annulation_reservation_voyage.annulation_reservation_voyage.configurations.JwtService;
 import com.annulation_reservation_voyage.annulation_reservation_voyage.models.User;
 import com.annulation_reservation_voyage.annulation_reservation_voyage.services.UserService;
@@ -35,10 +36,22 @@ public class UtilisateurController {
   private JwtService jwtService;
 
   @PostMapping("/inscription")
-  public ResponseEntity<User> inscription(@RequestBody User user) {
+  @Operation(summary = "Sign up a user")
+  public ResponseEntity<User> inscription(@RequestBody UserDTO user) {
 
-    User user2 = this.userService.create(user);
-    return new ResponseEntity<>(user, HttpStatus.OK);
+    User user1 = new User();
+
+    user1.setNom(user.getNom());
+    user1.setAddress(user.getAddress());
+    user1.setEmail(user.getEmail());
+    user1.setIdcoordonneeGPS(user.getIdcoordonneeGPS());
+    user1.setPassword(user.getPassword());
+    user1.setRole(user.getRole());
+    user1.setTelNumber(user.getTelNumber());
+    user1.setPrenom(user.getPrenom());
+
+    User user2 = this.userService.create(user1);
+    return new ResponseEntity<>(user2, HttpStatus.OK);
   }
 
   @GetMapping(path = "/connexion", consumes = MediaType.APPLICATION_JSON_VALUE)
