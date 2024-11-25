54c72443d82c93eab4dfa9df1a3d1f7f0a2a722f
Ngoupaye Thierry <thierryngoupaye@icloud.com>
2024-11-25 14:29:01 +0100

    added news

--- src/components/availableTripsComponents/viewDetailsButton.tsx
+++ src/components/availableTripsComponents/viewDetailsButton.tsx
@@ -4,13 +4,13 @@
 import {Button} from "@/components/ui/button";
 import {useRouter} from "next/navigation";
 
-export function ViewDetailsButton ()
+export function ViewDetailsButton ({tripId}: {tripId: string})
 {
     const router = useRouter();
 
     return (
         <Button variant="tertiary"
-                onClick= { ()=> { router.push("/availableTrips/trips") }}>
+                onClick= { ()=> { router.push(`/availableTrips/${tripId}`) }}>
             View Details
         </Button>
     );
