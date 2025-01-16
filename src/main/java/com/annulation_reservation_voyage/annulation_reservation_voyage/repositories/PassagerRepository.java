ea4dbd1a8aa60bfe8066aea3ad6f91398fb13a0b
Tchassi Daniel <tchassidaniel@gmail.com>
2025-01-16 06:10:22 +0100

    fix bug

--- src/main/java/com/annulation_reservation_voyage/annulation_reservation_voyage/repositories/PassagerRepository.java
+++ src/main/java/com/annulation_reservation_voyage/annulation_reservation_voyage/repositories/PassagerRepository.java
@@ -3,6 +3,7 @@ package com.annulation_reservation_voyage.annulation_reservation_voyage.reposito
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ import com.annulation_reservation_voyage.annulation_reservation_voyage.models.Pa
 @Repository
 public interface PassagerRepository extends CassandraRepository<Passager, UUID> {
 
+  @AllowFiltering
   List<Passager> findAllByIdReservation(UUID idReservation);
 }
