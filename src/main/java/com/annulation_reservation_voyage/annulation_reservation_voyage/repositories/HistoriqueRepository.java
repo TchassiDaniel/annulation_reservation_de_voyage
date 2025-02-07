3b2ba73ea7e9f6690e2b654c6bac3f665a2b7f2d
Jonathan Fomekong <jonathabachelard@gmail.com>
2025-02-07 12:53:07 +0100

    fix annulation error

--- src/main/java/com/annulation_reservation_voyage/annulation_reservation_voyage/repositories/HistoriqueRepository.java
+++ src/main/java/com/annulation_reservation_voyage/annulation_reservation_voyage/repositories/HistoriqueRepository.java
@@ -3,6 +3,7 @@ package com.annulation_reservation_voyage.annulation_reservation_voyage.reposito
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,7 @@ import com.annulation_reservation_voyage.annulation_reservation_voyage.models.Hi
 @Repository
 public interface HistoriqueRepository extends CassandraRepository<Historique, UUID> {
 
+    @AllowFiltering
     Optional<Historique> findByIdReservation(UUID idReservation);
 
 }
