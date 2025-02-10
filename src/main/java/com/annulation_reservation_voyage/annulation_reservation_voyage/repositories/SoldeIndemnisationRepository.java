8c22783da2f13952371fd202bc9699b13c2371d6
Tchassi Daniel <tchassidaniel@gmail.com>
2025-02-10 20:04:25 +0100

    résoudre le problème de allowfiltering sur solde indemnisation

--- src/main/java/com/annulation_reservation_voyage/annulation_reservation_voyage/repositories/SoldeIndemnisationRepository.java
+++ src/main/java/com/annulation_reservation_voyage/annulation_reservation_voyage/repositories/SoldeIndemnisationRepository.java
@@ -3,6 +3,7 @@ package com.annulation_reservation_voyage.annulation_reservation_voyage.reposito
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,7 @@ import com.annulation_reservation_voyage.annulation_reservation_voyage.models.So
 @Repository
 public interface SoldeIndemnisationRepository extends CassandraRepository<SoldeIndemnisation, UUID> {
 
+    @AllowFiltering
     Optional<SoldeIndemnisation> findByIdUserAndIdAgenceVoyage(UUID idUser, UUID idAgenceVoyage);
 
 }
